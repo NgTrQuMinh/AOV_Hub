00bf8c2d332af6594a8955c24b37bb38d545e890
Tran Gia Bao <trangiabao5606@gmail.com>
2026-09-18 10:27:39 +0700

    delete file

--- AOV-HUB/js/compare.js
+++
@@ -1,8 +0,0 @@
-/**
- * compare.js - Hỗ trợ trang So sánh Tướng
- * Phụ trách: Người 4
- *
- * TODO: đọc data/heroes.json (dùng loadData.js), 2 ô select, bảng so sánh
- * HP/Attack/Defense/Difficulty, highlight chỉ số cao hơn, dùng storage.js
- * để lưu/đọc aov_compare
- */
--- AOV-HUB/js/search.js
+++
@@ -1,6 +0,0 @@
-/**
- * search.js - Hỗ trợ tìm kiếm phía client
- * Phụ trách: Người 4
- *
- * TODO: gợi ý nhanh, bỏ dấu tiếng Việt, debounce
- */
